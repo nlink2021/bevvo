82979e76676279c3c9351d08512bd1b8cc837ba3
unknown <nlink2021@gmail.com>
2022-10-19 00:54:24 -0500

    second

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your alllpp!</Text>
+      <Text>Open up App.js to start working on your al ok llpp!</Text>
       <StatusBar style="auto" />
     </View>
   );
