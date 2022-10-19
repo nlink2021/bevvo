b94483ed41ddac2f8b2ededf0e9dd18ffd3c946f
unknown <nlink2021@gmail.com>
2022-10-19 01:09:36 -0500

    navigator start

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Card } from 'react-native-paper';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
